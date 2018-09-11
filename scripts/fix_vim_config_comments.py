94ab1ddeef3ba19cd82de6c8b648ec6f9b458ebb
ennmichael <enntheprogrammer@gmail.com>
2018-09-11 12:14:09 +0200

    Fixed config comments

--- scripts/fix_vim_config_comments.py
+++ scripts/fix_vim_config_comments.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3.6
+# vim: set shiftwidth=4 tabstop=4:
+
+
+'''
+This script adds vim config comments to files. It won't fix already existing config comments.
+'''
+
 
 import itertools
+import os.path
 from glob import glob
 
 
@@ -22,10 +30,11 @@ def get_extname(path):
 
 
 def source_files():
-    return itertools.chain(glob('src/*'), glob('tests/*'), glob('scripts/*'))
+    entries = itertools.chain(glob('src/*'), glob('tests/*'), glob('scripts/*'))
+    return (e for e in entries if os.path.isfile(e))
 
 
-def add_vim_config_comment(path):
+def fix_file(path):
     extname = get_extname(path)
     lines = read_lines(path)
     if not has_vim_config_comment(extname, lines):
@@ -43,14 +52,20 @@ def has_vim_config_comment(extname, lines):
 
 
 def add_vim_config_comment(extname, lines):
-    config_line = vim_config_comment(extname) + '\n'
-    if not config_line:
+    config_comment = vim_config_comment(extname)
+    if not config_comment:
         return False
+    config_line = config_comment + '\n'
     if extname == 'py':
         lines.insert(1, config_line)
     else:
-        lines.insert(0, config_line)
+        lines.insert(0, config_line + '\n')
     return True
+
+
+def fix_vim_config_comment(extname, lines):
+    i = 1 if extname == 'py' else 0
+    lines[i] = vim_config_comment(extname)
 
 
 def read_lines(path):
@@ -64,5 +79,6 @@ def write_lines(path, lines):
 
 
 if __name__ == '__main__':
-    for path in source_files:
+    for path in source_files():
+        fix_file(path)
 
